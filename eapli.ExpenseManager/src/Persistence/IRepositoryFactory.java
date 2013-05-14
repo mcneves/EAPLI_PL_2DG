6e4c297ab6a57e5be3f29fd2c0c0a1a23f643d81
Ricardo Rcha <1100662@isep.ipp.pt>
2013-05-14 17:27:36 +0100

    GERAL – Permitir Persistence em memória e em h2 db usando JPA 2.0 – (uso dos padrões Abstract Factory, Singleton, Strategy)
    Ricardo Rocha 1100662

--- eapli.ExpenseManager/src/Persistence/IRepositoryFactory.java
+++ eapli.ExpenseManager/src/Persistence/IRepositoryFactory.java
@@ -4,16 +4,13 @@
  */
 package Persistence;
 
-import Persistence.Inmemory.ExpenseRepository;
-import Persistence.Inmemory.ExpenseTypeRepository;
-import Persistence.Inmemory.PaymentMeansRepository;
 
 /**
  *
  * @author Ricardo Rocha
  */
 public interface IRepositoryFactory {
-    ExpenseRepository getExpenseRepository();
-    ExpenseTypeRepository getExpenseTypeRepository();
-    PaymentMeansRepository getPaymentMeansRepository();
+    IExpenseRepository getExpenseRepository();
+    IExpenseTypeRepository getExpenseTypeRepository();
+    IPaymentMeansRepository getPaymentMeansRepository();
 }
--- eapli.ExpenseManager/src/Persistence/JpaRepositoryFactory.java
+++ eapli.ExpenseManager/src/Persistence/JpaRepositoryFactory.java
@@ -8,6 +8,21 @@ package Persistence;
  *
  * @author Ricardo Rocha
  */
-public interface JpaRepositoryFactory{
+public  class JpaRepositoryFactory implements IRepositoryFactory{
+
+    @Override
+    public IExpenseRepository getExpenseRepository() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public IExpenseTypeRepository getExpenseTypeRepository() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public IPaymentMeansRepository getPaymentMeansRepository() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
