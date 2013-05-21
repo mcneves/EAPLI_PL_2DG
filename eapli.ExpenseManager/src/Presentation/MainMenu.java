5e69588bf05da57c24391f48aa8b5147c019bf51
1020091-josenuno <outsider.outminder@gmail.com>
2013-05-21 17:28:20 +0100

    correcção da classe MainMenu

--- eapli.ExpenseManager/src/Presentation/MainMenu.java
+++ eapli.ExpenseManager/src/Presentation/MainMenu.java
@@ -30,7 +30,7 @@ public class MainMenu {
                     break;
                 case 2:
                     ShowExpensesUI showExpensesUI = new ShowExpensesUI();
-                    showExpensesUI.loop();
+                    showExpensesUI.run();
                     break;
                 case 3:
                     ExpenseTypeRegisterUI uiType = new ExpenseTypeRegisterUI();
